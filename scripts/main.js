eb62bbfbc65fca936a0baafbcfd6549aa144f42c
AdrianaLuc <135138779+AdrianaLuc@users.noreply.github.com>
2023-05-31 17:13:40 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+/* Cookies */
+
+var cookies = document.getElementById('cookie_plate');
+var heading = document.getElementById('heading');
+
+cookies.onclick = function changeHeading() {
+    heading.textContent = 'Sorry, die sind nicht echt :('
+}
